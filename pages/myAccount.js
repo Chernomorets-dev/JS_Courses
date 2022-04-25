51572ba1b5815f1fb8d481eedd3783cbc16b2e2d
OLEH CHERNOMORETS <chernomorets.dev@gmail.com>
2022-04-25 17:45:07 +0300

    sadf

--- pages/myAccount.js
+++ pages/myAccount.js
@@ -1,7 +1,8 @@
+const { I } = inject();
+
 Feature('MyAccount');
 
 Scenario('test text', ({ I }) => {
-    I.amOnPage('http://automationpractice.com/index.php?controller=my-account');
-    I.see('Welcome to your account. Here you can manage all of your personal information and orders.');
-    //I.waitForVisible(locator);
+     I.see('Welcome to your account. Here you can manage all of your personal information and orders.');
+    
 });
