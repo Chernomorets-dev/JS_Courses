14da499872307c03f3603915e28f3df0690ee166
OLEH CHERNOMORETS <chernomorets.dev@gmail.com>
2022-04-25 13:06:51 +0300

    commit

---
+++ store_test.js
@@ -0,0 +1,9 @@
+const { locator } = require("codeceptjs");
+
+Feature('Store');
+Scenario('test something', ({ I }) => {
+I.amOnPage ('http://automationpractice.com/index.php');
+I.see ('Women');
+//I.waitForVisible(locator);
+pause();
+});
