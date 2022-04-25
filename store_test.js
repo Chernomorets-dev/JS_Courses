618170c2d586747ff2273ba46489e3181d697444
OLEH CHERNOMORETS <chernomorets.dev@gmail.com>
2022-04-25 15:18:55 +0300

    new pages

--- store_test.js
+++ store_test.js
@@ -1,9 +1,21 @@
-const { locator } = require("codeceptjs");
+let user = {
+    firstName: 'Test',
+    lastName: 'Automation',
+    state: 'Alabama',
+    city: 'Birmingham',
+    address: '801 Tom Martin Dr.',
+    postalCode: '35211',
+    
+}
 
 Feature('Store');
-Scenario('test something', ({ I }) => {
-I.amOnPage ('http://automationpractice.com/index.php');
-I.see ('Women');
-//I.waitForVisible(locator);
-pause();
+
+Scenario('test something', ({ I, homePage, authPage, createAccountPage }) => {
+    homePage.openStore();
+    homePage.clickSignIn();
+    authPage.fillNewUserEmail(Date.now() + '@test.com');
+    authPage.clickCreateAccount();
+    createAccountPage.fillNewUserForm(user);
+    pause();
+    I.see('My Account');
 });
