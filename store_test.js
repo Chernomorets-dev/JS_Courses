ea33644dd368a7d03bd056038ae17dc6886ce780
OLEH CHERNOMORETS <chernomorets.dev@gmail.com>
2022-05-02 01:06:11 +0300

    main (commit on 'main')

--- store_test.js
+++ store_test.js
@@ -7,10 +7,10 @@ let user = {
     address: '801 Tom Martin Dr.',
     phone: '+12163547758',
     postalCode: '35211',
-    email: '12163547758@test.com',
+    email: '12163547759@test31.com',
     password: 'passwordtest',
     days: '1',
-    months: '1',
+    months: '1',    
     years: '2000',
 }
 
@@ -19,16 +19,24 @@ Feature('Store');
 Scenario('test something', ({ I, homePage, authPage, createAccountPage, myAccountPage }) => {
     homePage.openStore();
     homePage.clickSignIn();
-    authPage.fillNewUserEmail('12163547758@test.com');
+    authPage.fillNewUserEmail('12163547759@test31.com');
     authPage.clickCreateAccount();
     createAccountPage.fillNewUserForm(user);
     myAccountPage.waitForPageLoad(); 
     myAccountPage.checkMyAccountPageIsVisible();
+    
+
+
+    
+    
+});
+
+After(({ I,  authPage, myAccountPage }) => {
     myAccountPage.clickSignOutButton();
     authPage.fillCurrentUserForm(user);
+   pause(); 
 
-
-    pause(); 
+    
     
     
 });
